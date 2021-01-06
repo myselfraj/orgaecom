d5a8058df714cea2e2bfac9d5e8ef2ccd4079f17
yujratnesh <ratnesh@yujdesigns.com>
2021-01-06 15:57:30 +0530

    First commit

---
+++ app/src/main/java/com/quintus/labs/grocerystore/api/clients/RestClient.java
@@ -0,0 +1,94 @@
+package com.quintus.labs.grocerystore.api.clients;
+
+
+import android.content.Context;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.quintus.labs.grocerystore.api.LoggingInterceptor;
+import com.quintus.labs.grocerystore.api.RestService;
+import com.quintus.labs.grocerystore.api.ToStringConverterFactory;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+/**
+ * RetrofitExample
+ * https://github.com/quintuslabs/RetrofitExample
+ * Created on 18-Feb-2019.
+ * Created by : Santosh Kumar Dash:- http://santoshdash.epizy.com
+ */
+
+public class RestClient {
+
+    public static final String BASE_URL = "http://rashienterprises.in/ecomm/"; //"https://megagrocerystore.000webhostapp.com/";//http://<ipaddress>/directoryname
+    public static Retrofit RETROFIT = null;
+    public static Retrofit RETROFIT1 = null;
+    public static RestService restService = null;
+
+    public static Retrofit getClient() {
+        if (RETROFIT == null) {
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(new LoggingInterceptor())
+                    .build();
+            RETROFIT = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return RETROFIT;
+    }
+
+    public static Retrofit getStringClient() {
+        if (RETROFIT1 == null) {
+            RETROFIT1 = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(new ToStringConverterFactory())
+                    .build();
+        }
+        return RETROFIT1;
+    }
+
+
+    /*val httpLoggingInterceptor: HttpLoggingInterceptor
+    get() {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG) {
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        } else {
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.NONE
+        }
+        return httpLoggingInterceptor
+    }*/
+
+    public static HttpLoggingInterceptor httpLoggingInterceptor(){
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        return httpLoggingInterceptor;
+    }
+    public static RestService getRestService(final Context context) {
+        if (restService == null) {
+            Gson gson = new GsonBuilder().setLenient().create();
+            OkHttpClient.Builder builder = new OkHttpClient().newBuilder();
+            builder.readTimeout(30, TimeUnit.SECONDS);
+            builder.connectTimeout(30, TimeUnit.SECONDS);
+            builder.addInterceptor(httpLoggingInterceptor());
+            OkHttpClient client = builder.build();
+
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+            restService = retrofit.create(RestService.class);
+        }
+        return restService;
+    }
+
+}
